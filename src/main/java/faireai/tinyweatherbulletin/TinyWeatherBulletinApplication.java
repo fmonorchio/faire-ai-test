154ff1186ff177afe63c2424a2b27495b7d5942f
fmonorchio <francesco.monorchio@gruppomol.it>
2021-11-08 15:10:09 +0100

    Configured Spring Boot

---
+++ src/main/java/faireai/tinyweatherbulletin/TinyWeatherBulletinApplication.java
@@ -0,0 +1,13 @@
+package faireai.tinyweatherbulletin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyWeatherBulletinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyWeatherBulletinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/faireai/tinyweatherbulletin/TinyWeatherBulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package faireai.tinyweatherbulletin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyWeatherBulletinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
